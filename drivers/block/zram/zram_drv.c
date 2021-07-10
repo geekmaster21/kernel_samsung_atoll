6061112d03a8343b8c7d154d9909e944263c176f
topser9 <topser181@gmail.com>
2021-07-11 01:07:34 +0700

    Enable zram support and set default compressor as lz4

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -62,7 +62,7 @@ static const char *default_compressor = "lz4";
 #elif IS_ENABLED(CONFIG_CRYPTO_ZSTD)
 static const char *default_compressor = "zstd";
 #else
-static const char *default_compressor = "lzo-rle";
+static const char *default_compressor = "lz4";
 #endif
 
 /* Module params (documentation at end) */
